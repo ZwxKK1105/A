7cbee68c52974b08698fbf46902ab49ae184acb5
ZwxKK1105 <3396519140@qq.com>
2026-01-24 20:13:45 +0800

    Update EnemyCombatComponent.cpp

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,13 +2,52 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
 
 #include"WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor))
 	{
-		Debug::Print(GetOwningPawn()->GetHumanReadableName() + TEXT(" is hitting") + HitActor->GetActorNameOrLabel());
+		//Debug::Print(GetOwningPawn()->GetHumanReadableName() + TEXT(" is hitting") + HitActor->GetActorNameOrLabel());
+		
+		return;
 	}
+
+	OverlappedActors.AddUnique(HitActor);
+
+
+	//格挡检查
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
+	{
+		//检查格挡是否有效
+
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;	
+
+	if (bIsValidBlock)
+	{
+		//成功格挡
+
+
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
+	}
+
 }
